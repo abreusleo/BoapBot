8ab5fb3fd7368129003e403eccfaad0612461c0a
Leonardo Santos Abreu <leonardo.abreu@globo.com>
2021-07-27 17:30:37 -0300

    Testing Vscode git

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MyClient(discord.Client):
         logging.info('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        logging.info('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
 client.run('NDQ2MTI1MzE2MzY5Mjg1MTIw.WvuMlA.CkKirbpz0bxq2tkqq40UE6h_oU0')
