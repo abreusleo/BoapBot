670a12e4a6abbb348ae6fa6f4b4126a8fd5f413c
Leonardo Santos Abreu <leonardo.abreu@globo.com>
2021-07-27 21:25:24 -0300

    Command prefix is now on .env

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import os
 import discord
 import logging
 import ModCommands as modCommands
-from dotenv import load_dotenv
+import dotenv
+
+def choose_command_prefix(prefixList, id):
+    for el in prefixList:
+        if el[0] == id:
+            return el[1]
+    return prefixList[0][1]
 
 class MyClient(discord.Client):
+    dotenv.load_dotenv()
+    commandPrefix = os.getenv('DISCORD_COMMAND_PREFIX')
+
     async def bot_error(self, command, channel):
         logging.error("{} error.".format(command))
         await channel.send('Comando inválido.') 
@@ -13,7 +22,7 @@ class MyClient(discord.Client):
         # TODO Check if this is the best way to log something
         logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
         logging.info('Boap is ready!')
-
+    
     async def on_message(self, message):
         author = message.author
         channel = message.channel
@@ -21,13 +30,12 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        if message.content.startswith('!bulk'):
+        if message.content.startswith(self.commandPrefix + 'bulk'):
             try:
                 await modCommands.bulk(message)
             except:
                 await self.bot_error("Bulk delete", channel)
 
-load_dotenv()
 client = MyClient()
 TOKEN = os.getenv('DISCORD_TOKEN') # Get Token from .env file
 client.run(TOKEN)
